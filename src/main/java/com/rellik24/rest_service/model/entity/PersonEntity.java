ea06de2e280c772d66aeb67c68f6bbdaaa79e028
rellik <pan820124@gmail.com>
2024-10-04 00:40:25 +0800

    refactor: 💡 replace javax with jarkata

--- src/main/java/com/rellik24/rest_service/model/entity/PersonEntity.java
+++ src/main/java/com/rellik24/rest_service/model/entity/PersonEntity.java
@@ -2,13 +2,16 @@ package com.rellik24.rest_service.model.entity;
 
 import lombok.Data;
 
-import javax.persistence.*;
+import jakarta.persistence.*;
 
 @Data
 @Entity
 @Table(name = "person", schema = "public") // 指定表名，與資料庫的 person 表對應
 public class PersonEntity {
     @Id
+    @Column(name = "id")
+    private String id;
+
     @Column(name = "name")
     private String name;
 
